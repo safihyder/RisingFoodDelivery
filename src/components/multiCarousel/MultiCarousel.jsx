f0190a22ff220833ca9acfb4f4715a991f4ef6ab
Safi Hyder <safihaider0987@gmail.com>
2025-03-11 22:22:16 +0530

    fix: adjust mobile breakpoint and remove unnecessary div in Items screen

--- src/components/multiCarousel/MultiCarousel.jsx
+++ src/components/multiCarousel/MultiCarousel.jsx
@@ -15,7 +15,7 @@ const responsive = {
     slidesToSlide: 3 // optional, default to 1.
   },
   mobile: {
-    breakpoint: { max: 767, min: 400 },
+    breakpoint: { max: 767, min: 300 },
     items: 1,
     slidesToSlide: 1 // optional, default to 1.
   }
--- src/screens/Items/Items.jsx
+++ src/screens/Items/Items.jsx
@@ -4,7 +4,6 @@ const Items = ({ items }) => {
     return (
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-6 justify-items-center">
-
             {items ? items.map((item, index) => {
                 return <Card key={index} item={item} />
             }) : <div />
